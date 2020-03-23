c67d6292ea16d37ae020addaf909610492a24e3a
William <ome450901@gmail.com>
2020-03-23 23:07:05 +1100

    Added Square initialization

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,13 +1,30 @@
 package main.java.model;
 
+/**
+ * @author WeiYi Yu
+ * @date 2020-03-23
+ */
 public class Board {
 
-    private int wide;
-    private int area;
+    private static final int ROW = 10;
 
-    public Board(int wide) {
-        this.wide = wide;
-        area = wide * wide;
+    private Square[] squares = new Square[ROW * ROW];
+
+    public Board() {
+        initSquare();
+    }
+
+    private void initSquare() {
+        for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < ROW; col++) {
+                Square square = new Square();
+                squares[row * ROW + col] = square;
+            }
+        }
+    }
+
+    public Square getSquareAt(int row, int col) {
+        return squares[row * ROW + col];
     }
 
 }
