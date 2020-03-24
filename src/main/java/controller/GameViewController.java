34794290d1f9fd17f84c7ca5283f1e855d210f1e
William <ome450901@gmail.com>
2020-03-24 23:06:15 +1100

    Implemented BoardView event listener

--- src/main/java/controller/GameViewController.java
+++ src/main/java/controller/GameViewController.java
@@ -1,19 +1,26 @@
 package main.java.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import main.java.view.BoardView;
+import main.java.view.BoardView.BoardViewListener;
 
 /**
  * @author WeiYi Yu
  * @date 2020-03-23
  */
-public class GameViewController {
+public class GameViewController
+        implements BoardViewListener {
 
     @FXML
-    private Label lbHelloWorld;
+    private BoardView boardView;
 
     @FXML
     public void initialize() {
-        lbHelloWorld.setText("Hey Yo");
+        boardView.setBoardViewListener(this);
+    }
+
+    @Override
+    public void onSquareClick(int row, int col) {
+        boardView.changeSquareColor(row, col);
     }
 }
--- src/main/java/view/BoardView.java
+++ src/main/java/view/BoardView.java
@@ -6,28 +6,34 @@ import javafx.scene.Node;
 import javafx.scene.layout.*;
 import main.java.model.Board;
 
+import java.util.Objects;
+
 import static main.java.Game.WINDOW_HEIGHT;
 import static main.java.Game.WINDOW_WIDTH;
 
 public class BoardView
         extends GridPane {
 
+    public interface BoardViewListener {
+        void onSquareClick(int row, int col);
+    }
+
+    private BoardViewListener boardViewListener;
+
     public BoardView() {
         super();
         drawBoard(Board.ROW);
     }
 
+    // Just testing
+    public void changeSquareColor(int row, int col) {
+        getSquareAt(row, col).setStyle("-fx-background-color: red;");
+    }
+
     private void drawBoard(int boardSize) {
         for (int row = 0; row < boardSize; row++) {
             for (int col = 0; col < boardSize; col++) {
-                StackPane square = new StackPane();
-                String color;
-                if ((row + col) % 2 == 0) {
-                    color = "white";
-                } else {
-                    color = "black";
-                }
-                square.setStyle("-fx-background-color: " + color + ";");
+                StackPane square = getSquare(row, col);
                 add(square, col, row);
             }
         }
@@ -48,12 +54,37 @@ public class BoardView
         }
     }
 
-    private Node getSquare(int row, int col) {
+    private StackPane getSquare(int row, int col) {
+        StackPane square = new StackPane();
+        String color;
+        if ((row + col) % 2 == 0) {
+            color = "white";
+        } else {
+            color = "black";
+        }
+        square.setStyle("-fx-background-color: " + color + ";");
+        square.setOnMouseClicked(event -> getBoardViewListener().onSquareClick(row, col));
+        return square;
+    }
+
+    private Node getSquareAt(int row, int col) {
         for (Node child : getChildren()) {
             if (GridPane.getRowIndex(child) == row && GridPane.getColumnIndex(child) == col) {
                 return child;
             }
         }
         return null;
+    }
+
+    public BoardViewListener getBoardViewListener() {
+        return Objects.requireNonNull(boardViewListener);
+    }
+
+    public void setBoardViewListener(BoardViewListener boardViewListener) {
+        if (boardViewListener == null) {
+            throw new NullPointerException("Must provide a non-null listener.");
+        }
+
+        this.boardViewListener = boardViewListener;
     }
 }
