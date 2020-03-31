82efbebe8389ab8420e068d4e0a75aa37276c075
William <ome450901@gmail.com>
2020-04-01 10:51:56 +1100

    Removed BoardViewController and moved methods into GameViewController

--- src/main/java/controller/GameViewController.java
+++ src/main/java/controller/GameViewController.java
@@ -2,13 +2,16 @@ package main.java.controller;
 
 import javafx.fxml.FXML;
 import main.java.model.Board;
+import main.java.model.piece.Piece;
 import main.java.view.BoardView;
+import main.java.view.BoardView.BoardViewEventListener;
 
 /**
  * @author WeiYi Yu
  * @date 2020-03-23
  */
-public class GameViewController {
+public class GameViewController
+        implements BoardViewEventListener {
 
     @FXML
     private BoardView boardView;
@@ -18,8 +21,14 @@ public class GameViewController {
     @FXML
     public void initialize() {
         board = new Board(boardView);
+    }
 
-        // Propagate BoardView event into BoardViewController
-        boardView.setBoardViewEventListener(new BoardViewController(board, boardView));
+    @Override
+    public void onSquareClicked(int row, int col) {
+        Piece piece = board.getPiece(row, col);
+//        if (piece != null) {
+//            allValidMoves = piece.getAllMoves(row, col);
+//            boardView.showValidMoves(allValidMoves);
+//        }
     }
 }
