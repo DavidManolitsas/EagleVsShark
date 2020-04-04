4cf5601ca5d6c9ab3068923c4c09ec0205790a41
William <ome450901@gmail.com>
2020-04-04 23:22:50 +1100

    Added GameInfoViewListener interface

--- src/main/java/controller/GameViewController.java
+++ src/main/java/controller/GameViewController.java
@@ -2,19 +2,28 @@ package main.java.controller;
 
 import javafx.fxml.FXML;
 import main.java.model.Board;
+import main.java.model.move.Move;
 import main.java.model.piece.Piece;
 import main.java.view.BoardView;
 import main.java.view.BoardView.BoardViewEventListener;
+import main.java.view.GameInfoView;
+import main.java.view.GameInfoView.GameInfoViewEventListener;
+
+import java.util.List;
 
 /**
  * @author WeiYi Yu
  * @date 2020-03-23
  */
 public class GameViewController
-        implements BoardViewEventListener {
+        implements BoardViewEventListener,
+                   GameInfoViewEventListener {
 
     @FXML
     private BoardView boardView;
+
+    @FXML
+    private GameInfoView gameInfoView;
 
     private Board board;
 
@@ -22,14 +31,25 @@ public class GameViewController
     public void initialize() {
         board = new Board(boardView);
         boardView.setBoardViewEventListener(this);
+        gameInfoView.setGameInfoViewEventListener(this);
     }
 
+    //region BoardView Event
     @Override
     public void onSquareClicked(int row, int col) {
         Piece piece = board.getPiece(row, col);
-//        if (piece != null) {
-//            allValidMoves = piece.getAllMoves(row, col);
-        boardView.showValidMoves(row, col);
-//        }
+
+        if (piece != null) {
+            List<Move> allMoves = piece.getAllMoves(row, col);
+            gameInfoView.showValidMoveList(allMoves);
+        }
     }
+    //endregion
+
+    //region GameInfoView Event
+    @Override
+    public void onMoveClicked(Move move) {
+
+    }
+    //endregion
 }
