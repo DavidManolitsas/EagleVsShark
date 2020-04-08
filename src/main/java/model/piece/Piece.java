8cdd1b0999d8d7d6ee9fb2516da313d2840b033d
Jess <s3763636@student.rmit.edu.au>
2020-04-08 13:59:39 +1000

    changed getAllMoves in Piece to protected

--- src/main/java/model/piece/Piece.java
+++ src/main/java/model/piece/Piece.java
@@ -21,7 +21,7 @@ public abstract class Piece {
         allMovesList = new ArrayList<Move>();
     }
 
-    public abstract List<Move> getAllMoves(int startRow, int startCol);
+    protected abstract List<Move> getAllMoves(int startRow, int startCol);
 
     public List<Move> getMovesList(int startRow, int startCol) {
         if (allMovesList.isEmpty()) {
