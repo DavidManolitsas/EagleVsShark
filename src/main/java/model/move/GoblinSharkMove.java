61208901f8f5c9ed41a810f53f6fc7761082c49c
William <ome450901@gmail.com>
2020-04-16 14:32:20 +1000

    Modified GoblinSharkMove with movements

--- src/main/java/model/move/GoblinSharkMove.java
+++ src/main/java/model/move/GoblinSharkMove.java
@@ -1,134 +1,31 @@
 package main.java.model.move;
 
+import main.java.model.move.movements.*;
+import main.java.model.move.shape.SquareShape;
+
+import java.util.ArrayList;
 import java.util.List;
 
-// UPDATE: The goblin shark can go left, diagonal left, diagonal right, right,
-// it can not go up and down and can only go one step.
-// it still cover 8 square around it, but go diagonal it can take 5 squares each time
+public class GoblinSharkMove {
 
-public class GoblinSharkMove extends Move {
+    private List<Move> moveList;
 
-    public GoblinSharkMove(int startRow, int startCol, int squaresMoved, String direction) {
-        super(startRow, startCol, squaresMoved, direction);
-        checkAndAttach();
-    }
+    public GoblinSharkMove(int startRow, int startCol, int squaresMoved) {
+        moveList = new ArrayList<>();
 
+        Movements[] movements = {
+                new MoveLeft(startRow, startCol, squaresMoved),
+                new MoveRight(startRow, startCol, squaresMoved),
+                new MoveDiagonalLeft(startRow, startCol, squaresMoved),
+                new MoveDiagonalRight(startRow, startCol, squaresMoved)
+        };
 
-    private void checkAndAttach(){
-        // generate all info base on the direction
-        switch (direction) {
-            case "down":
-                break;
-
-            case "up":
-                break;
-
-            case "left":
-                // execute each command
-                for (String command : COMMANDS) {
-                    switch (command) {
-                        case PAINT:
-                            paintGeneration();
-                            break;
-
-                        case DEST:
-                            // the destination will be check first
-                            // record destination coordinate base on the steps and direction
-                            destination[0] = startRow;
-                            destination[1] = startCol - squaresMoved;
-                            break;
-
-                        case ROUTE:
-                            // record route based on the steps and direction
-                            for (int col = startCol; col >= destination[1]; col--) {
-                                route.add(new int[]{startRow, col});
-                            }
-                            break;
-                    }
-                }
-                break;
-            case "right":
-                // execute each command
-                for (String command : COMMANDS) {
-                    switch (command) {
-                        case PAINT:
-                            paintGeneration();
-                            break;
-
-                        case DEST:
-                            // the destination will be check first
-                            // record destination coordinate base on the steps and direction
-                            destination[0] = startRow;
-                            destination[1] = startCol + squaresMoved;
-                            break;
-
-                        case ROUTE:
-                            // record route based on the steps and direction
-                            for (int col = startCol; col <= destination[0]; col++) {
-                                route.add(new int[]{startRow, col});
-                            }
-                            break;
-                    }
-                }
-                break;
-
-            case "diagonal up left":
-                for (String command : COMMANDS) {
-                    switch (command) {
-                        case PAINT:
-                            paintGeneration();
-                            break;
-
-                        case DEST:
-                            destination[0] = startRow - squaresMoved;
-                            destination[1] = startCol - squaresMoved;
-                            break;
-
-                        case ROUTE:
-                            int col = startCol;
-                            for (int row = startRow; row >= destination[0]; row--, col--) {
-                                route.add(new int[]{row, col});
-                            }
-                            break;
-                    }
-                }
-                break;
-            case "diagonal up right":
-                for (String command : COMMANDS) {
-                    switch (command) {
-                        case PAINT:
-                            paintGeneration();
-                            break;
-
-                        case DEST:
-                            destination[0] = startRow - squaresMoved;
-                            destination[1] = startCol + squaresMoved;
-                            break;
-
-                        case ROUTE:
-                            int col = startCol;
-                            for (int row = startRow; row >= destination[0]; row--, col++) {
-                                route.add(new int[]{row, col});
-                            }
-                            break;
-                    }
-                }
-                break;
+        for (Movements movement : movements) {
+            moveList.add(new Move(movement, new SquareShape(movement.getDestination())));
         }
     }
 
-    // record the coordinates for the squares around the piece
-    private void paintGeneration(){
-        // paint the three squares at the top of the piece
-        for (int col = destination[1] - 1; col <= destination[1] + 1; col++) {
-            paintInfo.add(new int[]{destination[0] - 1, col});
-        }
-        // paint the three squares at the bottom of the piece
-        for (int col = destination[1] - 1; col <= destination[1] + 1; col++) {
-            paintInfo.add(new int[]{destination[0] + 1, col});
-        }
-        // paint the two squares on the left and right of the piece
-        paintInfo.add(new int[]{destination[0], destination[1] - 1});
-        paintInfo.add(new int[]{destination[0], destination[1] + 1});
+    public List<Move> getMoveList() {
+        return moveList;
     }
 }
