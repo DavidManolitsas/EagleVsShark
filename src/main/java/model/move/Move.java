1f3b0b09de37f5f3bb17153b9f962e1fba66d74d
William <ome450901@gmail.com>
2020-04-16 14:33:23 +1000

    Removed unused variables and add movements

--- src/main/java/model/move/Move.java
+++ src/main/java/model/move/Move.java
@@ -1,42 +1,28 @@
 package main.java.model.move;
 
-import java.util.ArrayList;
+import main.java.model.move.movements.Movements;
+import main.java.model.move.shape.PaintShape;
+
 import java.util.List;
 
-public abstract class Move {
-    protected final String PAINT = "paint";
-    protected final String DEST = "dest";
-    protected final String ROUTE = "route";
-    protected final String[] COMMANDS = {DEST, PAINT, ROUTE};
+public class Move {
+    private Movements movements;
+    private PaintShape paintShape;
 
-    protected int startRow;
-    protected int startCol;
-    protected int[] destination = new int[2];
-    protected int squaresMoved;
-    protected String direction;
-    protected List<int[]> paintInfo = new ArrayList<>();
-    protected List<int[]> route = new ArrayList<>();
-
-    protected Move(int startRow, int startCol, int squaresMoved, String direction) {
-        this.startRow = startRow;
-        this.startCol = startCol;
-        this.squaresMoved = squaresMoved;
-        this.direction = direction;
+    public Move(Movements movements, PaintShape paintShape) {
+        this.movements = movements;
+        this.paintShape = paintShape;
     }
 
-    public List<int[]> getPaintInfo() {
-        return paintInfo;
-    }
-
-    public void setPaintInfo(List<int[]> paintInfo) {
-        this.paintInfo = paintInfo;
-    }
-
-    public List<int[]> getRoute() {
-        return route;
+    public PaintShape getPaintShape() {
+        return paintShape;
     }
 
     public int[] getFinalPosition() {
-        return destination;
+        return movements.getDestination();
+    }
+
+    public List<int[]> getRoute() {
+        return movements.getRoute();
     }
 }
--- src/main/java/model/piece/Piece.java
+++ src/main/java/model/piece/Piece.java
@@ -9,13 +9,6 @@ import java.util.List;
 public abstract class Piece {
     protected List<Move> allMovesList;
 
-    public static final String DIRECTION_DOWN = "down";
-    public static final String DIRECTION_UP = "up";
-    public static final String DIRECTION_LEFT = "left";
-    public static final String DIRECTION_RIGHT = "right";
-    public static final String DIRECTION_DIAGONAL_UP_RIGHT = "diagonal up right";
-    public static final String DIRECTION_DIAGONAL_UP_LEFT = "diagonal up left";
-
     // Initial position of piece
     private int[] startPos;
 
@@ -24,13 +17,7 @@ public abstract class Piece {
         allMovesList = new ArrayList<Move>();
     }
 
-    protected abstract List<Move> getAllMoves(int startRow, int startCol);
-
-    public List<Move> getMovesList(int startRow, int startCol) {
-        allMovesList.clear();
-        getAllMoves(startRow, startCol);
-        return allMovesList;
-    }
+    public abstract List<Move> getAllMoves(int startRow, int startCol);
 
     /**
      * @return Initial position of the piece
