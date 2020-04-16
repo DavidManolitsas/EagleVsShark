1f0f76491d1a0b2ba5aa8c5c63d995a08b386493
William <ome450901@gmail.com>
2020-04-16 14:32:30 +1000

    Modified GoldenEagleMove with movements

--- src/main/java/model/move/GoldenEagleMove.java
+++ src/main/java/model/move/GoldenEagleMove.java
@@ -1,34 +1,26 @@
 package main.java.model.move;
 
+import main.java.model.move.movements.MoveBehindPiece;
+import main.java.model.move.shape.TriangleShape;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class GoldenEagleMove extends Move {
-    private int[] sharkPos = new int[2];
+public class GoldenEagleMove {
 
-    public GoldenEagleMove(int startRow, int startCol, int[] sharkPos) {
-        super(startRow, startCol, 0 , "");
-        this.sharkPos = sharkPos;
-        checkAndAttach();
+    private List<Move> moveList;
+
+    public GoldenEagleMove(int startRow, int startCol, List<int[]> piecePositionList) {
+        moveList = new ArrayList<>();
+
+        for (int[] positions : piecePositionList) {
+            moveList.add(new Move(new MoveBehindPiece(startRow, startCol, positions[0], positions[1]),
+                                  new TriangleShape(startRow, startCol, TriangleShape.DIRECTION_LEFT)));
+        }
+
     }
 
-    private void checkAndAttach(){
-        // determine destination based on the shark location
-        destination[0] = sharkPos[0] + 1;
-        destination[1] = sharkPos[1];
-
-        // add starting point and destination into route
-        route.add(new int[]{startRow, startCol});
-        route.add(destination);
-
-        // add paint area based on the destination and shark position
-
-        // paint at the destination point
-        paintInfo.add(new int[]{destination[0], destination[1]});
-        // paint on the left and right side of the shark
-        paintInfo.add(new int[]{sharkPos[0], sharkPos[1] - 1});
-        paintInfo.add(new int[]{sharkPos[0], sharkPos[1] + 1});
-        // paint on the left and right behind the destination
-        paintInfo.add(new int[]{destination[0] + 1, destination[1] - 1});
-        paintInfo.add(new int[]{destination[0] + 1, destination[1] + 1});
+    public List<Move> getMoveList() {
+        return moveList;
     }
 }
