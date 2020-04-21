428c268fae5038dc2ed99900d697f50b2ea0ad39
s3630517 <s3630517@student.rmit.edu.au>
2020-04-21 15:50:01 +1000

    Renamed MoveGenerator to PieceMove

--- src/main/java/model/move/SawSharkMove.java
+++ src/main/java/model/move/SawSharkMove.java
@@ -5,17 +5,16 @@ import main.java.model.move.movements.MoveDiagonalRight;
 import main.java.model.move.shape.TriangleShape;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class SawSharkMove extends MoveFactory{
+public class SawSharkMove extends PieceMove {
 
     private static final int SQUARE_MOVED = 2;
 
     public SawSharkMove(int startRow, int startCol) {
         moveList = new ArrayList<>();
         moveList.add(new Move(new MoveDiagonalLeft(startRow, startCol, SQUARE_MOVED),
-                              new TriangleShape(startRow, startCol, TriangleShape.DIRECTION_LEFT)));
+                new TriangleShape(startRow, startCol, TriangleShape.DIRECTION_LEFT)));
         moveList.add(new Move(new MoveDiagonalRight(startRow, startCol, SQUARE_MOVED),
-                              new TriangleShape(startRow, startCol, TriangleShape.DIRECTION_RIGHT)));
+                new TriangleShape(startRow, startCol, TriangleShape.DIRECTION_RIGHT)));
     }
 }
